083cb2bd6681efb6fe795a9971265a1930f72ca0
LucasGabrielfl0 <lucasgabrielfl00@gmail.com>
2024-10-11 14:53:17 -0300

    adding basics of CAN communication with the VCU

---
+++ lib/GeneralCAN/GeneralCAN.h
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+#define VCU_MOTOR_ID 1
+#define VCU_GYRO_ID 2
+#define BMS_ID 3
+#define TCU_ID 4
+
+uint8_t VCU_Msg[8];
+
+
+struct MotorData{
+    float Supply_Voltage{0};        // Supply Voltage (V)
+    uint8_t Temp_M1{0};             // Temperature, Motor 1 (°C)
+    uint8_t Temp_M2{0};             // Temperature, Motor 2 (°C)
+    uint8_t Current_M1{0};          // Phase Current, Motor 1 (°C)
+    uint8_t Current_M2{0};          // Phase Current, Motor 2 (°C)
+    uint16_t RPM_M1{0};             // Velocity, Motor 1 (RPM)
+};
+
+void get() {
+    MotorData Motor;
+
+    Motor.Supply_Voltage = (VCU_Msg[1]<< 8) | VCU_Msg[9];
+    Motor.Temp_M1    = VCU_Msg[2]-100;
+    Motor.Temp_M2    = VCU_Msg[3]-100;
+    Motor.Current_M1 = VCU_Msg[4];
+    Motor.Current_M2 = VCU_Msg[5];
+    Motor.RPM_M1     = (VCU_Msg[7]<< 8) | VCU_Msg[6];
+    
+    int id;
+    switch (id){
+        case VCU_MOTOR_ID:
+
+            break;
+        
+        case VCU_GYRO_ID:
+            break;
+        
+        case BMS_ID:
+
+            break;
+    }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
-#include <Arduino.h>
+/***
+ * Capibarib E-racing
+ * Federal University of Pernambuco (UFPE)
+ * Group Area: Data Acquisition
+ 
+ * This file contains the code for the cars TCU (TELEMATIC CONTROL UNIT) that unites the:
+ * 1. CAN communication with the VCU and the BMS
+ * 2. The Data Logger 
+ * 3. The vehicle's Display
+ ***/
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+uint8_t VCU_Msg[8];
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void read(){
+
+    int Supply_Voltage = (VCU_Msg[1]<< 8) | VCU_Msg[9];
+    int Temp_M1    = VCU_Msg[2]-100;
+    int Temp_M2    = VCU_Msg[3]-100;
+    int Current_M1 = VCU_Msg[4];
+    int Current_M2 = VCU_Msg[5];
+    int RPM_M1     = (VCU_Msg[7]<< 8) | VCU_Msg[6];
 }
